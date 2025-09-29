cf1c90be3a665b790b6648d59377d81caaef2c3a
Horldsence <m18511047688@163.com>
2025-09-29 20:23:11 +0800

    Modified Slint build configuration to use Fluent style.

--- crates/ui_main/build.rs
+++ crates/ui_main/build.rs
@@ -1,6 +1,6 @@
 use slint_build::CompilerConfiguration;
 
 fn main() {
-    let c = CompilerConfiguration::new().with_style("cupertino".to_string());
+    let c = CompilerConfiguration::new().with_style("fluent".to_string());
     slint_build::compile_with_config("src/app.slint", c).unwrap();
 }
