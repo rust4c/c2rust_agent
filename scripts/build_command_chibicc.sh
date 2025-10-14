f2e2b67c9f3c7810710533159854a75f49a40488
Horldsence <m18511047688@163.com>
2025-10-15 00:25:12 +0800

    Updated test auto cli

---
+++ scripts/build_command_chibicc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /tmp/translate_chibicc
+cargo build --release
--- scripts/codegen_command_chibicc.sh
+++ scripts/codegen_command_chibicc.sh
@@ -1,1 +1,3 @@
-#! bash
+#!/bin/bash
+cd /opt/c2rust_agent
+cargo run -- translate --input-dir translate_chibicc/src --output-dir /tmp/translate_chibicc --debug
