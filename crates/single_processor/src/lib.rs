80016571774e1d3df154554b8e09e5734698982d
ContartSL <226798292@qq.com>
2025-10-18 16:28:02 +0800

    The two-stage translation feature has been delete.

--- crates/single_processor/src/lib.rs
+++ crates/single_processor/src/lib.rs
@@ -1,6 +1,6 @@
 // 模块声明
 
-pub mod c2rust_translator;
+
 
 pub mod file_processor;
 pub mod pkg_config;
@@ -8,7 +8,7 @@ pub mod rust_verifier;
 pub mod single_processes;
 
 // 主要处理函数
-pub use single_processes::{StageCallback, two_stage_processor_with_callback};
+pub use single_processes::{StageCallback, singlefile_processor};
 
 // 导出各模块的公共函数
 pub use rust_verifier::{extract_key_errors, verify_and_fix, verify_compilation};
